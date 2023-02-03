32fc5e96926d6c2a00bbd7c4ff3a2ec735e21dea
Raphael Ugwu <ugwuraphael@gmail.com>
2023-02-03 23:41:19 +0000

    updated ListScreen.js

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const friends = [
+    {name: 'Friend #1'},
+    {name: 'Friend #2'},
+    {name: 'Friend #3'},
+    {name: 'Friend #4'},
+    {name: 'Friend #5'},
+    {name: 'Friend #6'},
+    {name: 'Friend #7'},
+    {name: 'Friend #8'},
+    {name: 'Friend #9'},
+]
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>
-};
+    return (
+        <FlatList
+        data={friends}
+        renderItem={({item}) => {
+            return <Text>{item.name}</Text>
+        }}
+        />
+    )
+}
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default ListScreen;
